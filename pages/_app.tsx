18b7cf7b0f62667dc38185e2d5edb748813ee30e
Wesley-SdS <wesleysantos.0095@gmail.com>
2023-10-04 09:52:51 -0300

    Car card update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 import { StyleProvider, ThemePicker } from 'vcc-ui';
-import { HelloWorld } from '../src/components/HelloWorld';
+
+import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
 
-
-function HomePage(){
-  return(
+function HomePage() {
+  return (
     <React.StrictMode>
       <StyleProvider>
-        <ThemePicker variant='light'>
-          <HelloWorld />
-        </ThemePicker>
+      <ThemePicker variant="light">
+        <HelloWorld />
+      </ThemePicker>
       </StyleProvider>
     </React.StrictMode>
   );
 }
-
 
 export default HomePage;
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { Block, Button } from "vcc-ui";
+import { Text } from "vcc-ui";
+import { useCars } from "../hooks/useCars";
+import { CarCard } from "./CarCard";
+
+import styles from '../../public/css/home.module.css'
+import { Spacer } from "./Spacer";
 
 export const HelloWorld: React.FC = () => {
+  const { cars } = useCars();
+
   return (
-
-    <Block extend={{ padding: 20 }}>
-    <Button>Click me!</Button>
-  </Block>
-
-  )
+    <div className={styles.homeWrapper}>
+      <Text variant="cook">Todos os modelos Recharge</Text>
+      <Spacer></Spacer>
+      <div className={styles.cardsWrapper}>
+        {cars.map(car => <CarCard key={car.id} car={car}/>)}
+      </div>
+    </div>
+  );
 };
